481f36c58d661a8a914822e88a8768a4db6939d7
Jorge Vicente <81838137+011Jorge@users.noreply.github.com>
2021-10-22 19:40:55 -0300

    mega-movies
    Site de filmes

---
+++ script.js
@@ -0,0 +1,19 @@
+// let genero = document.getElementById("genero");
+// genero.addEventListener("mouseenter", entrouGenero);
+// genero.addEventListener("mouseout", saiuGenero);
+
+// function entrouGenero() {
+//     let res = document.getElementById("respostaGenero");
+//     res.innerHTML = "Olá"
+//     res.style.background="#0E0E0E"
+//     res.style.display = "flex"
+//     res.style.width = "100px" 
+//     res.style.height = "auto"
+//     res.style.color = "#fff"
+// }
+
+// function saiuGenero() {
+//     let res = document.getElementById("respostaGenero");
+//     res.innerHTML = ""
+//     res.style.display = "none"
+// }   
